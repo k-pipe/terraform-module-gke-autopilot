f647a4f0ca43e415c8898e22cc5447e97ce4e055
Jan Kneissler <jan.kneissler@breuninger.de>
2024-09-18 21:42:54 +0200

    unify provider definitions to become identical

--- provider.tf
+++ provider.tf
@@ -12,17 +12,17 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-    host                   = google_container_cluster.k8s-cluster.endpoint
-    cluster_ca_certificate = base64decode(google_container_cluster.k8s-cluster.master_auth[0].cluster_ca_certificate)
-    token                  = data.google_client_config.default.access_token
+      host                   = "https://${google_container_cluster.k8s-cluster.endpoint}"
+      cluster_ca_certificate = base64decode(google_container_cluster.k8s-cluster.master_auth[0].cluster_ca_certificate)
+      token                  = data.google_client_config.default.access_token
   }
 }
 
 provider "kubectl" {
   load_config_file       = false
-  host                   = "https://${data.google_container_cluster.k8s-cluster.endpoint}"
-  token                  = "${data.google_container_cluster.k8s-cluster.access_token}"
-  cluster_ca_certificate = "${base64decode(data.google_container_cluster.k8s-cluster.master_auth.0.cluster_ca_certificate)}"
+  host                   = "https://${google_container_cluster.k8s-cluster.endpoint}"
+  cluster_ca_certificate = base64decode(google_container_cluster.k8s-cluster.master_auth[0].cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
 }
 
 terraform {
