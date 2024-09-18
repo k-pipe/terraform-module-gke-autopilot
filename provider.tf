3f0da5a86c2d2d1c848dc8a52893d2913ddf31b0
Jan Kneissler <jan.kneissler@breuninger.de>
2024-09-18 21:31:41 +0200

    fix cluster name

--- provider.tf
+++ provider.tf
@@ -20,9 +20,9 @@ provider "helm" {
 
 provider "kubectl" {
   load_config_file       = false
-  host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
-  token                  = "${data.google_container_cluster.my_cluster.access_token}"
-  cluster_ca_certificate = "${base64decode(data.google_container_cluster.my_cluster.master_auth.0.cluster_ca_certificate)}"
+  host                   = "https://${data.google_container_cluster.k8s-cluster.endpoint}"
+  token                  = "${data.google_container_cluster.k8s-cluster.access_token}"
+  cluster_ca_certificate = "${base64decode(data.google_container_cluster.k8s-cluster.master_auth.0.cluster_ca_certificate)}"
 }
 
 terraform {
